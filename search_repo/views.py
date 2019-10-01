6d370233e18c1e68188e996db2836888f70188f9
BMaheshwari <bhupali.maheshwari@gmail.com>
2019-10-01 16:21:55 +0530

    request

--- search_repo/views.py
+++ search_repo/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+import requests
 
 # Create your views here.
 from django.views.generic import FormView
@@ -8,3 +9,7 @@ class search_repository(FormView):
     form_class = SearchForm
     template_name = 'search_repo/search.html'
 
+    def form_valid(self, form):
+        url_link = form.cleaned_data.get('title')
+        response = requests.get(url_link)
+        print(response)
