181200f50b528e887a70be68eedd881f8cad4d37
BMaheshwari <bhupali.maheshwari@gmail.com>
2019-10-03 19:23:54 +0530

    Stats of contributor added

--- search_repo/views.py
+++ search_repo/views.py
@@ -1,18 +1,16 @@
 import json
 
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render
 import requests
 
 # Create your views here.
-from django.urls import reverse_lazy
 from django.views.generic import FormView, TemplateView
 from .forms import SearchForm
 
 class search_repository(FormView):
     form_class = SearchForm
     template_name = 'search_repo/search.html'
-    success_url = reverse_lazy('DisplayView')
 
     def post(self, request, *args, **kwargs):
         form = self.get_form()
@@ -20,35 +18,24 @@ class search_repository(FormView):
 
         if form.is_valid():
             url_link = form.cleaned_data.get('title')
-            print("In form valid", url_link)
-
             base_url = "https://api.github.com/repos/"
 
-            new_url_link = url_link.strip().replace("https://github.com/", '')
-            print(new_url_link)
-
-            user_name = list(map(str, new_url_link.split('/')))
-            print(user_name)
-
-            repo_link = base_url + user_name[0] + '/' + user_name[1] + '/contributors?per_page=10'
-            print(repo_link)
-            contributors_link = requests.get(repo_link)
-
-            response_contributors = contributors_link.json()
-
-            # print(response_contributors)
-
-            # distros_dict = json.loads(response_contributors)
-
-            context['contributors'] = response_contributors
-
-            return render(request, 'search_repo/display.html',context)
-            # return self.render_to_response(context)
-
-    def form_valid(self, form):
-        return HttpResponseRedirect(self.get_success_url())
+            try:
+                new_url_link = url_link.strip().replace("https://github.com/", '')
+                user_name = list(map(str, new_url_link.split('/')))
+                repo_link = base_url + user_name[0] + '/' + user_name[1] + '/stats/contributors'
+            except:
+                return HttpResponse("<h1>No such repository found</h1>")
+            else:
+                contributors_link = requests.get(repo_link)
+                response_contributors = contributors_link.json()
+                try:
+                    if response_contributors['message'] == "Not Found":
+                        return HttpResponse("<h1>This github url does not exist</h1>")
+                except:
+                    context['contributors'] = response_contributors
+                    return render(request, 'search_repo/display.html',context)
 
 
-class DisplayView(TemplateView):
-    template_name = 'search_repo/display.html'
+
 
