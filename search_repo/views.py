985fb2dcf89ae5e7ce69e8cbe9af2e3fdb26488e
BMaheshwari <bhupali.maheshwari@gmail.com>
2019-10-23 13:51:14 +0530

    committing views.py

--- search_repo/views.py
+++ search_repo/views.py
@@ -1,20 +1,13 @@
 from collections import defaultdict
-<<<<<<< HEAD
-=======
 import time
 from django.core.exceptions import ObjectDoesNotExist
->>>>>>> model_branch
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render
 import requests
 from .models import History, Commit, Contributor
 
 # Create your views here.
-<<<<<<< HEAD
-from django.views.generic import FormView, TemplateView
-=======
 from django.views.generic import FormView, TemplateView, ListView
->>>>>>> model_branch
 from .forms import SearchForm
 
 class search_repository(FormView):
@@ -24,39 +17,6 @@ class search_repository(FormView):
     def post(self, request, *args, **kwargs):
         form = self.get_form()
         context = self.get_context_data(**kwargs)
-<<<<<<< HEAD
-        users_dict = defaultdict(list)
-
-        if form.is_valid():
-            url_link = form.cleaned_data.get('title')
-            base_url = "https://api.github.com/"
-
-            try:
-                new_url_link = url_link.strip().replace("https://github.com/", '')
-                user_name = list(map(str, new_url_link.split('/')))
-                repo_link = base_url + 'repos/' + user_name[0] + '/' + user_name[1] + '/stats/contributors'
-            except:
-                return HttpResponse("<h1>No such repository found</h1>")
-            else:
-                contributors_link = requests.get(repo_link)
-                if contributors_link.status_code == 404:
-                    return HttpResponse("<h1>This github url does not exist</h1>")
-                elif contributors_link.status_code == 403:
-                    return HttpResponse("<h1>API rate Limit exceeded</h1>")
-                else:
-                    response_contributors = contributors_link.json()
-                    l = len(response_contributors)
-                    contributors = response_contributors[l:l - 11:-1]
-                    context['contributors'] = contributors
-                    return render(request, 'search_repo/display.html',context)
-
-        else:
-            return HttpResponse("<h1>Invalid URL</h1>")
-
-
-
-
-=======
         if "api_fetch" in request.POST:
             if form.is_valid():
                 repo_url_link = form.cleaned_data.get('title')
@@ -113,4 +73,3 @@ class search_repository(FormView):
                 context['contributors_set'] = dict1
 
                 return render(request, 'search_repo/display_from_database.html',context)
->>>>>>> model_branch
